5e0632cdcd16e3fb4edea2d343f8fa626d7b5eca
czapek1337 <czapek1337@gmail.com>
2021-10-16 22:43:43 +0000

    fix the stupid oversight thanks to Avery from osdev discord

--- kernel/acpi/acpi.h
+++ kernel/acpi/acpi.h
@@ -22,7 +22,7 @@ struct [[gnu::packed]] sdt_header_t {
     uint8_t revision;
     uint8_t checksum;
     char oem_id[6];
-    char oem_table_id[6];
+    char oem_table_id[8];
     uint32_t oem_revision;
     uint32_t creator_id;
     uint32_t creator_revision;

bda28cd40c28a0a9fdb30b78130a959e37ecddc1
czapek1337 <czapek1337@gmail.com>
2021-10-17 05:19:15 +0200

    implement a simple dynamic vector

---
+++ kernel/ds/vector.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <stdint.h>
+
+template <typename T>
+class vector_t {
+private:
+    T *m_values;
+
+    uint64_t m_capacity;
+    uint64_t m_count;
+
+public:
+    vector_t(uint64_t capacity = 0) {
+        m_values = nullptr;
+        m_capacity = capacity;
+        m_count = 0;
+    }
+
+    ~vector_t() {
+        if (m_values)
+            delete[] m_values;
+    }
+
+    void push(T value) {
+        if (m_count >= m_capacity) {
+            if (m_capacity == 0)
+                m_capacity = 1;
+
+            m_capacity *= 2;
+
+            auto new_values = new T[m_capacity];
+
+            for (auto i = 0; i < m_count; i++)
+                new_values[i] = m_values[i];
+
+            if (m_values)
+                delete[] m_values;
+
+            m_values = new_values;
+        }
+
+        m_values[m_count++] = value;
+    }
+
+    void pop() {
+        if (m_count == 0)
+            return;
+
+        m_count--;
+    }
+
+    T &operator[](uint64_t index) { return m_values[index]; }
+    T &at(uint64_t index) { return m_values[index]; }
+
+    const T &operator[](uint64_t index) const { return m_values[index]; }
+    const T &at(uint64_t index) const { return m_values[index]; }
+
+    uint64_t size() const { return m_count; }
+    uint64_t capacity() const { return m_capacity; }
+};

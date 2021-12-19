86019e1364358e7696444fe624716b2b5e055b8a
YusufKhan-human <54283715+YusufKhan-gamedev@users.noreply.github.com>
2021-12-19 00:05:22 -0800

    Push fs to Userland

--- kernel/src/fs/fd.h
+++ kernel/src/fs/fd.h
@@ -1,14 +1,1 @@
-#pragma once
-
-#include <stdint.h>
-
-#include "vfs.h"
-
-namespace vfs::fd {
-
-uint64_t allocate(VfsOpenedFile *file);
-uint64_t duplicate(uint64_t old_fd);
-
-void destroy(uint64_t fd);
-
-} // namespace vfs::fd
+#include "../../../user/fs/fd.h"

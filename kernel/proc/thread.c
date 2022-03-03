6d5d8bc69b8fd5d6d8df7ae5c672d005b9250f3f
czapek1337 <czapek1337@gmail.com>
2022-03-03 17:10:41 +0100

    Separate out the ELF loading from thread creation

--- kernel/proc/thread.c
+++ kernel/proc/thread.c
@@ -29,6 +29,45 @@ static void thread_common_setup(thread_t *thread, vaddr_t entry, size_t stack_pa
   }
 }
 
+static vaddr_t thread_load_elf(thread_t *thread, elf64_header_t *elf, vaddr_t load_offset, bool is_user) {
+  elf64_program_header_t *phdrs = (elf64_program_header_t *) ((uintptr_t) elf + elf->e_phoff);
+
+  vaddr_t entry = elf->e_entry;
+
+  for (size_t i = 0; i < elf->e_phnum; i++) {
+    elf64_program_header_t *phdr = &phdrs[i];
+
+    if (phdr->p_type == PT_LOAD) {
+      vaddr_t virt_start = load_offset + ALIGN_DOWN(phdr->p_vaddr, 4096);
+      vaddr_t virt_end = load_offset + ALIGN_UP(phdr->p_vaddr + phdr->p_memsz, 4096);
+      vaddr_t virt_file_end = load_offset + ALIGN_UP(phdr->p_vaddr + phdr->p_filesz, 4096);
+
+      size_t size = virt_end - virt_start;
+      size_t file_size = virt_file_end - virt_start;
+
+      uint64_t map_flags = PTE_P | (is_user ? PTE_U : 0);
+
+      if (IS_SET(phdr->p_flags, PF_W))
+        map_flags |= PTE_W;
+
+      if (IS_CLEAR(phdr->p_flags, PF_X))
+        map_flags |= PTE_NX;
+
+      paddr_t phys_segment = phys_alloc(ALIGN_UP(size, 4096) / 4096);
+      vaddr_t virt_segment = phys_to_virt(phys_segment);
+
+      memset((void *) virt_segment, 0, size);
+      memcpy((void *) virt_segment, (const void *) ((uintptr_t) elf + phdr->p_offset), file_size);
+
+      vm_map(thread->vm, virt_start, phys_segment, size, map_flags);
+    } else if (phdr->p_type == PT_INTERP) {
+      // TODO: Load the rtdl
+    }
+  }
+
+  return entry;
+}
+
 thread_t *thread_create(vaddr_t entry, bool is_user) {
   thread_t *thread = ALLOC_ZERO(thread_t);
 
@@ -46,33 +85,9 @@ thread_t *thread_create_elf(elf64_header_t *elf, bool is_user) {
   thread->vm = virt_new_vm();
   thread->regs = ALLOC_ZERO(isr_frame_t);
 
-  elf64_program_header_t *phdrs = (elf64_program_header_t *) ((uintptr_t) elf + elf->e_phoff);
+  vaddr_t entry = thread_load_elf(thread, elf, 0, is_user);
 
-  for (size_t i = 0; i < elf->e_phnum; i++) {
-    elf64_program_header_t *phdr = &phdrs[i];
-
-    if (phdr->p_type != PT_LOAD)
-      continue;
-
-    size_t page_count = ALIGN_UP(phdr->p_memsz, 4096) / 4096;
-    uint64_t map_flags = PTE_P | (is_user ? PTE_U : 0);
-
-    if (IS_SET(phdr->p_flags, PF_W))
-      map_flags |= PTE_W;
-
-    if (IS_CLEAR(phdr->p_flags, PF_X))
-      map_flags |= PTE_NX;
-
-    paddr_t segment_phys = phys_alloc(page_count);
-    vaddr_t segment_virt = phys_to_virt(segment_phys);
-
-    memset((void *) segment_virt, 0, phdr->p_memsz);
-    memcpy((void *) segment_virt, (void *) ((uintptr_t) elf + phdr->p_offset), phdr->p_filesz);
-
-    vm_map(thread->vm, phdr->p_vaddr, segment_phys, page_count * KIB(4), map_flags);
-  }
-
-  thread_common_setup(thread, elf->e_entry, 2, is_user);
+  thread_common_setup(thread, entry, 2, is_user);
 
   return thread;
 }
